8defea6e4117ed20d9b0e94a4976a5b9d50f8820
Brenno Murakami <brenno.murakami@gmail.com>
2023-11-26 01:20:54 -0300

    Transformando arquivo em .h e renomeando com base na especificação do projeto

---
+++ BrennoMurakami.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <conio.h>
+
+int x = 1, y = 1;
+
+// Função para posicionar o cursor na tela
+void gotoxy(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+}
+
+void atualizar_cursor(int nx, int ny) {
+    gotoxy(nx, ny);
+}
+
+void monitoramento_teclas(){
+    char entrada[256];  // Array para armazenar a entrada do usuário
+    int posicao = 0, tecla;    // Posição atual na entrada
+    printf("(Pressione 'ESC' para sair):\n");
+    do {
+        if (_kbhit()) {
+            tecla = _getch();
+
+            //ESC
+            if (tecla == 27) {
+                printf("Tecla 'ESC' pressionada. Finalizando o programa.\n");
+                break;
+            }
+
+            if(tecla == 13){
+                y++;
+                atualizar_cursor(x, y);
+            }
+
+            //Backspace
+            if (tecla == 8 && posicao > 0) {
+                printf("\b \b");  // Move o cursor uma posição para trás e apaga o caractere
+                posicao--;
+                entrada[posicao] = '\0';  // Remove o último caractere da entrada
+            } else if(tecla != 224 || tecla != 0 || tecla != 134){
+                printf("%c", tecla);
+                entrada[posicao] = tecla;  // Armazena o caractere na entrada
+                posicao++;
+            }
+
+            //Teclas especiais
+            if (tecla == 224 || tecla == 0) {
+                tecla = _getch();
+                switch (tecla){
+                    case 59:
+                        printf("\nTecla F1 pressionada.\n");
+                        atualizar_cursor(15, 15);
+                        break;
+                    case 60:
+                        printf("\nTecla F2 pressionada.\n");
+                        break;
+                    case 61:
+                        printf("\nTecla F3 pressionada.\n");
+                        break;
+                    case 62:
+                        printf("\nTecla F4 pressionada.\n");
+                        break;
+                    case 63:
+                        printf("\nTecla F5 pressionada.\n");
+                        break;
+                    case 64:
+                        printf("\nTecla F6 pressionada.\n");
+                        break;
+                    case 65:
+                        printf("\nTecla F7 pressionada.\n");
+                        break;
+                    case 66:
+                        printf("\nTecla F8 pressionada.\n");
+                        break;
+                    case 67:
+                        printf("\nTecla F9 pressionada.\n");
+                        break;
+                    case 68:
+                        printf("\nTecla F10 pressionada.\n");
+                        break;
+                    case 134:
+                        printf("\nTecla F12 pressionada.\n");
+                        break;
+                    case 77:
+                        printf("\nSeta Direita");
+                        break;
+                    case 80:
+                        printf("\nSeta Baixo");
+                        break;
+                    case 75:
+                        printf("\nSeta Esquerda");
+                        break;
+                    case 72:
+                        printf("\nSeta Cima");
+                        break;
+                    case 82:
+                        printf("\nInsert");
+                        break;
+                    case 83:
+                        printf("\nDelete");
+                        break;
+                    case 79:
+                        printf("\nEnd");
+                        break;
+                    case 71:
+                        printf("\nHome");
+                        break;
+                    case 73:
+                        printf("\nPageUP");
+                        break;
+                    case 81:
+                        printf("\nPageDown");
+                        break;
+                }
+            }
+        }
+    } while (1);
+}
+
+void main() {
+    gotoxy(x, y);
+    monitoramento_teclas();
+}
---
+++ BrennoMurakamiEstrutura.h
@@ -0,0 +1,255 @@
+#ifndef BrennoMurakamiEstrutura_H
+#define BrennoMurakamiEstrutura_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lista{
+    char linha[90];
+    int tam;
+    struct lista *next;
+}LISTA;
+
+void inicializar(LISTA **lista){
+    *lista = NULL;
+}
+
+LISTA* getnode(){
+    return (LISTA*)malloc(sizeof(LISTA));
+}
+
+void freenode(LISTA *q){
+    free(q);
+}
+
+int empty(LISTA **lista){
+    if(*lista == NULL){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void exibir(LISTA *lista){
+    while(lista != NULL){
+        for(int i = 0; i <= lista->tam; i++){
+            printf("%c", lista->linha[i]);
+        }
+        lista = lista->next;
+        printf("\n");
+    }
+}
+
+void inserir_caractere_final(LISTA **lista, char caractere, int no){
+    LISTA *q;
+    q = *lista;
+    int i = 0;
+
+    while(i != no){
+        q = q->next;
+        i++;
+    }
+    q->linha[q->tam+1] = caractere;
+    q->tam++;
+}
+
+void inserir_caractere_comeco(LISTA **lista, char caractere, int no){
+    LISTA *q;
+    q = *lista;
+    int i = 0;
+    while(i != no){
+        q = q->next;
+        i++;
+    }
+    if(q->tam == -1){
+        q->linha[0] = caractere;
+    }else{
+        int j = q->tam+1;
+        while(j > 0){
+            q->linha[j] = q->linha[j-1];
+            j--;
+        }
+        q->linha[j] = caractere;
+    }
+    q->tam++;
+}
+
+void inserir_caractere_posicao(LISTA **lista, char caractere, int no, int pos){
+    LISTA *q;
+    q = *lista;
+    int i = 0;
+    while(i != no){
+        q = q->next;
+        i++;
+    }
+    int j = q->tam+1;
+    while(j > pos){
+        j--;
+        q->linha[j+1] = q->linha[j];
+    }
+    q->linha[j] = caractere;
+    q->tam++;
+}
+
+void remover_caractere_posicao(LISTA **lista, int no, int pos){
+    LISTA *q;
+    q = *lista;
+    int i = 0;
+    while(i != no){
+        q = q->next;
+        i++;
+    }
+    for(int j = pos; j < q->tam; j++){
+        q->linha[j] = q->linha[j+1];
+    }
+    q->tam--;
+}
+
+void remover_caractere_comeco(LISTA **lista, int no){
+    LISTA *q;
+    q = *lista;
+    int i = 0;
+    while(i != no){
+        q = q->next;
+    }
+    for(int j = 0; j < q->tam; j++){
+        q->linha[j] = q->linha[j+1];
+    }
+    q->tam--;
+}
+
+void remover_caractere_final(LISTA **lista, int no){
+    LISTA *q;
+    q = *lista;
+    int i = 0;
+    while(i != no){
+        q = q->next;
+    }
+    q->tam--;
+}
+
+void criar_linha_comeco(LISTA **lista){
+    LISTA *q;
+    q = getnode();
+    if(q == NULL){
+        printf("\nHouve um erro.");
+        return;
+    }else{
+        q->tam = -1;
+        if(empty(lista)){
+            q->next = NULL;
+            *lista = q;
+        }
+        else{
+            q->next = *lista;
+            *lista = q;
+        }
+    }
+}
+
+void criar_linha_posicao(LISTA **lista, int pos){
+    LISTA *q, *aux;
+    q = getnode();
+    aux = *lista;
+    int i = 0;
+    if(q != NULL){
+        q->tam = -1;
+        if(aux->next == NULL){
+            q->next = NULL;
+            aux->next = q;
+        }
+        else{
+            while(i != pos){
+                aux = aux->next;
+                i++;
+            }
+            q->next = aux->next;
+            aux->next = q;
+        }
+    }
+    else{
+        printf("\nHouve um erro.");
+    }
+}
+
+void criar_linha_final(LISTA **lista){
+    LISTA *q;
+    q = getnode();
+    if(q == NULL){
+        printf("\nHouve um erro");
+        return;
+    }else{
+        q->tam = -1;
+        q->next = NULL;
+        if(empty(lista)){
+            *lista = q;
+        }else{
+            LISTA *aux;
+            aux = *lista;
+            while(aux->next != NULL){
+                aux = aux->next;
+            }
+            aux->next = q;
+        }
+    }
+}
+
+void remover_linha_comeco(LISTA **lista){
+    if(empty(lista)){
+        printf("\nLista vazia.");
+    }else{
+        LISTA *aux;
+        aux = *lista;
+        *lista = (*lista)->next;
+        free(aux);
+    }
+}
+
+void remover_linha_final(LISTA **lista){
+    if(empty(lista)){
+        printf("\nLista vazia.");
+    }else{
+        LISTA *aux, *q;
+        aux = *lista;
+        if((*lista)->next == NULL){
+            *lista = NULL;
+            freenode(aux);
+        }
+        else{
+            q = (*lista)->next;
+            while(q->next != NULL){
+                q = q->next;
+                aux = aux->next;
+            }
+            aux->next = NULL;
+            freenode(q);
+        }
+    }
+}
+
+void remover_linha_posicao(LISTA **lista, int pos){
+    if(empty(lista)){
+        printf("\nLista vazia.");
+    }
+    else{
+        LISTA *aux, *q;
+        aux = *lista;
+        if((*lista)->next == NULL){
+            *lista = NULL;
+            freenode(aux);
+        }
+        else{
+            q = (*lista)->next;
+            int i = 1;
+            while(i != pos){
+                q = q->next;
+                aux = aux->next;
+                i++;
+            }
+            aux->next = q->next;
+            free(q);
+        }
+    }
+}
+
+
+#endif // BrennoMurakamiEstrutura
