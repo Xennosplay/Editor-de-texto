f829239109fd06692209f35cdbed9237623e0a39
Brenno Murakami <brenno.murakami@gmail.com>
2023-11-29 04:00:53 -0300

    Retirada das variáveis x e y da tela declaradas globalmente para fins de teste, neste caso, declaração delas no main e passagem para atribuição de valores com base em ponteiro

--- BrennoMurakami.c
+++ BrennoMurakami.c
@@ -2,8 +2,6 @@
 #include <conio.h>
 #include "BrennoMurakamiEstrutura.h"
 
-int x = 1, y = 1;
-
 // Função para posicionar o cursor na tela
 void gotoxy(int x, int y) {
     printf("\033[%d;%dH", y, x);
@@ -17,34 +15,34 @@ void limpar_tela(){
     system("cls");
 }
 
-void aumentar_x(){
-    if(x < 90){
-        x++;
+void aumentar_x(int *x, int *y){
+    if(*x < 90){
+        (*x)++;
     }
     else{
-        y++;
-        x = 1;
+        (*y)++;
+        *x = 1;
     }
-    atualizar_cursor(x, y);
+    atualizar_cursor(*x, *y);
 }
 
-void diminuir_x(){
-    if(x > 1){
-        x--;
+void diminuir_x(int *x, int *y){
+    if(*x > 1){
+        (*x)--;
     }
-    else if(y > 1){
-        y--;
-        x = 90;
+    else if(*y > 1){
+        (*y)--;
+        *x = 90;
     }
-    atualizar_cursor(x, y);
+    atualizar_cursor(*x, *y);
 }
 
-void aumentar_y(){
-    y++;
-    atualizar_cursor(x, y);
+void aumentar_y(int *x, int *y){
+    (*y)++;
+    atualizar_cursor(*x, *y);
 }
 
-void monitoramento_teclas(LISTA **lista){
+void monitoramento_teclas(LISTA **lista, int *x, int *y){
     int tecla;
     LISTA *q, *aux;
     q = *lista;
@@ -70,69 +68,69 @@ void monitoramento_teclas(LISTA **lista){
                         break;
                     //SETA DIREITA
                     case 77:
-                        q = retornar_no_atual(*lista, y-1);
-                        if(x < q->tam+2 && q->linha[x-1] != '\n'){
-                            aumentar_x();
+                        q = retornar_no_atual(*lista, (*y)-1);
+                        if(*x < q->tam+2 && q->linha[(*x)-1] != '\n'){
+                            aumentar_x(x,y);
                         }
                         else{
                             if(q->next != NULL){
                                 q = q->next;
-                                y++;
-                                x = 1;
-                                atualizar_cursor(x, y);
+                                (*y)++;
+                                *x = 1;
+                                atualizar_cursor(*x, *y);
                             }
                         }
                         break;
                     //SETA BAIXO
                     case 80:
-                        q = retornar_no_atual(*lista, y-1);
+                        q = retornar_no_atual(*lista, (*y)-1);
                         if(q->next != NULL){
                             q = q->next;
-                            if(x-2 > q->tam){
+                            if((*x)-2 > q->tam){
                                 if(q->linha[q->tam] == '\n'){
-                                    x = q->tam+1;
+                                    *x = q->tam+1;
                                 }
                                 else{
-                                    x = q->tam+2;
+                                    *x = q->tam+2;
                                 }
                             }
-                            y++;
-                            atualizar_cursor(x, y);
+                            (*y)++;
+                            atualizar_cursor(*x, *y);
                         }
                         break;
                     //SETA ESQUERDA
                     case 75:
-                        if(y > 1){
-                            q = retornar_no_anterior(*lista, y-1);
-                            if(x > 1){
-                            x--;
+                        if(*y > 1){
+                            q = retornar_no_anterior(*lista, (*y)-1);
+                            if(*x > 1){
+                            (*x)--;
                             }
                             else{
                                 if(q->linha[q->tam] == '\n'){
-                                    x = q->tam+1;
+                                    *x = q->tam+1;
                                 }
                                 else{
-                                    x = q->tam+2;
+                                    *x = q->tam+2;
                                 }
-                                y--;
+                                (*y)--;
                             }
                         }
                         else{
-                            if(x > 1){
-                                x--;
+                            if(*x > 1){
+                                (*x)--;
                             }
                         }
-                        atualizar_cursor(x, y);
+                        atualizar_cursor(*x, *y);
                         break;
                     //SETA CIMA
                     case 72:
-                        if(y > 1){
-                            q = retornar_no_anterior(*lista, y-1);
-                            if(x > q->tam+1){
-                                x = q->tam+1;
+                        if(*y > 1){
+                            q = retornar_no_anterior(*lista, (*y)-1);
+                            if(*x > q->tam+1){
+                                *x = q->tam+1;
                             }
-                            y--;
-                            atualizar_cursor(x, y);
+                            (*y)--;
+                            atualizar_cursor(*x, *y);
                         }
                         break;
                     //INSERT
@@ -140,29 +138,36 @@ void monitoramento_teclas(LISTA **lista){
                         break;
                     //DELETE
                     case 83:
-                        remover_caractere_posicao(lista, y-1, x-1);
-                        limpar_tela();
-                        exibir(*lista);
-                        atualizar_cursor(x, y);
-                        break;
-                    //END
-                    case 79:
-                        q = retornar_no_atual(*lista, y-1);
-                        if(q->linha[q->tam] == '\n'){
-                            x = q->tam+1;
+                        q = retornar_no_atual(*lista, (*y)-1);
+                        if(q->linha[(*x)-1] != '\n'){
+                            remover_caractere_posicao(lista, (*y)-1, (*x)-1);
                         }
                         else{
-                            x = q->tam+2;
+
                         }
 
                         limpar_tela();
                         exibir(*lista);
-                        atualizar_cursor(x, y);
+                        atualizar_cursor(*x, *y);
+                        break;
+                    //END
+                    case 79:
+                        q = retornar_no_atual(*lista, (*y)-1);
+                        if(q->linha[q->tam] == '\n'){
+                            *x = q->tam+1;
+                        }
+                        else{
+                            *x = q->tam+2;
+                        }
+
+                        limpar_tela();
+                        exibir(*lista);
+                        atualizar_cursor(*x, *y);
                         break;
                     //HOME
                     case 71:
-                        x = 1;
-                        atualizar_cursor(x, y);
+                        *x = 1;
+                        atualizar_cursor(*x, *y);
                         break;
                     //PAGEUP
                     case 73:
@@ -175,49 +180,49 @@ void monitoramento_teclas(LISTA **lista){
 
                 //BACKSPACE
                 if (tecla == 8) {
-                    if(x == 1){
-                        if(y > 1){
-                            q = retornar_no_anterior(*lista, y-1);
-                            aux = retornar_no_atual(*lista, y-1);
+                    if(*x == 1){
+                        if(*y > 1){
+                            q = retornar_no_anterior(*lista, (*y)-1);
+                            aux = retornar_no_atual(*lista, (*y)-1);
 
                             if(aux->tam > -1){
 
                                 int i = 0;
-                                while( i <= aux->tam){
-                                    if(aux->linha[i] != '\n' && q->tam < 89){
-                                        inserir_caractere_posicao(lista, aux->linha[i], y-2, q->tam);
+                                while( i <= aux->tam && q->tam < 89){
+                                    if(aux->linha[i] != '\n'){
+                                        inserir_caractere_posicao(lista, aux->linha[i], (*y)-2, q->tam);
                                     }
                                     i++;
                                 }
 
                                 while( i >= 0){
                                     if(aux->linha[i] != '\n'){
-                                        remover_caractere_posicao(lista, y-1, i);
+                                        remover_caractere_posicao(lista, (*y)-1, i);
                                     }
                                     i--;
                                 }
                             }
                             if(aux->tam == -1 || aux->linha[0] == '\n'){
-                                remover_linha_posicao(lista,y-1);
+                                remover_linha_posicao(lista,(*y)-1);
                             }
                             limpar_tela();
                             exibir(*lista);
 
                             if(q->linha[q->tam] != '\n'){
-                                x = q->tam+2;
+                                *x = q->tam+2;
                             }
                             else{
-                                x = q->tam+1;
+                                *x = q->tam+1;
                             }
-                            y--;
-                            atualizar_cursor(x, y);
+                            (*y)--;
+                            atualizar_cursor(*x, *y);
                         }
                     }
                     else{
-                        remover_caractere_posicao(lista,y-1, x-2);
+                        remover_caractere_posicao(lista,(*y)-1, (*x)-2);
                         limpar_tela();
                         exibir(*lista);
-                        diminuir_x();
+                        diminuir_x(x, y);
                     }
 
                 }
@@ -227,43 +232,43 @@ void monitoramento_teclas(LISTA **lista){
                 }
                 //ENTER
                 else if(tecla == 13){
-                    q = retornar_no_atual(*lista, y-1);
+                    q = retornar_no_atual(*lista, (*y)-1);
 
                     if(q->linha[q->tam] != '\n'){
-                        inserir_caractere_posicao(lista,'\n', y-1, x-1);
+                        inserir_caractere_posicao(lista,'\n', (*y)-1, (*x)-1);
                     }
 
-                    criar_linha_posicao(lista, y-1);
+                    criar_linha_posicao(lista, (*y)-1);
 
                     q = q->next;
                     if(q->next != NULL){
-                        inserir_caractere_posicao(lista,'\n', y, x-1);
+                        inserir_caractere_posicao(lista,'\n', *y, (*x)-1);
                     }
                     limpar_tela();
                     exibir(*lista);
-                    y++;
-                    x = 1;
-                    atualizar_cursor(x, y);
+                    (*y)++;
+                    *x = 1;
+                    atualizar_cursor(*x, *y);
                 }
                 else{
-                    if(x >= 90){
-                        q = retornar_no_atual(*lista, y-1);
+                    if(*x >= 90){
+                        q = retornar_no_atual(*lista, (*y)-1);
 
                         if(q->linha[q->tam] != '\n'){
-                            inserir_caractere_posicao(lista,'\n', y-1, x-1);
+                            inserir_caractere_posicao(lista,'\n', (*y)-1, (*x)-1);
                         }else{
-                            criar_linha_posicao(lista, y-1);
-                            inserir_caractere_posicao(lista, '\n', y, 0);
+                            criar_linha_posicao(lista, (*y)-1);
+                            inserir_caractere_posicao(lista, '\n', *y, 0);
                         }
 
                         if(q->next == NULL){
                             criar_linha_final(lista);
                         }
                     }
-                    inserir_caractere_posicao(lista, tecla, y-1, x-1);
+                    inserir_caractere_posicao(lista, tecla, (*y)-1, (*x)-1);
                     limpar_tela();
                     exibir(*lista);
-                    aumentar_x();
+                    aumentar_x(x, y);
                 }
             }
         }
@@ -272,10 +277,12 @@ void monitoramento_teclas(LISTA **lista){
 
 void main() {
     LISTA *lista;
-
+    int x, y;
+    x = 1;
+    y = 1;
     gotoxy(x, y);
 
     inicializar(&lista);
     criar_linha_comeco(&lista);
-    monitoramento_teclas(&lista);
+    monitoramento_teclas(&lista, &x, &y);
 }
